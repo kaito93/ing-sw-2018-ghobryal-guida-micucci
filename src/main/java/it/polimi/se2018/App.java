a798f8ff44bfe5ce43bc1c839df5deb172c81736
kaito93 <samuele.guida@mail.polimi.it>
2018-04-10 17:38:00 +0200

    Initial commit

---
+++ src/main/java/it/polimi/se2018/App.java
@@ -0,0 +1,20 @@
+package it.polimi.se2018;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int blu;
+        System.out.println( "Hello World!" );
+
+
+    }
+
+
+
+
+}
