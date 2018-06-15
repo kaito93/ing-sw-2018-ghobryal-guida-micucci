1274a2b6b40b34782bfca3d257be16da1255c013
kaito93 <samuele.guida@mail.polimi.it>
2018-06-15 15:55:09 +0200

    bug fix su schema dei round

--- src/main/java/it/polimi/se2018/model/RoundSchemeCell.java
+++ src/main/java/it/polimi/se2018/model/RoundSchemeCell.java
@@ -4,59 +4,73 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-/** class Round Scheme Cell
- *  contains all the information about the dices stored because they are not used during the match
- *  @author Andrea Micucci
+/**
+ * class Round Scheme Cell
+ * contains all the information about the dices stored because they are not used during the match
+ *
+ * @author Andrea Micucci
  */
 
 public class RoundSchemeCell implements Serializable {
     private ArrayList<Dice> restOfStock;
 
-    /** class constructor
+    /**
+     * class constructor
      * return an object formed by an arraylist of dice
      */
-    public RoundSchemeCell(){
+    public RoundSchemeCell() {
         restOfStock = new ArrayList<>();
     }
-    
-    /** method that return the dices stored in one cell
+
+    /**
+     * method that return the dices stored in one cell
+     *
      * @return an arraylist of dices not used during the turn
      */
     //tested
-    public List<Dice> getRestOfStock(){
+    public List<Dice> getRestOfStock() {
         return restOfStock;
     }
-    
-    /** method that add an arraylist of dice to a single cell
-     * @param toAdd the arraylist contains the dices not used during the turn
-     */
-    //tested
-    public void setDices(List<Dice> toAdd){
-        restOfStock.addAll(toAdd);
-    }
-    
-    /** @deprecated
-     * method that search a dice of which is known the color and the value
+
+
+
+    /**
      * @param value integer of the value of the dice the player is searching
      * @param color color of the dice the player is searching
      * @return a dice Object if the dice exists in the arraylist, else null
+     * @deprecated method that search a dice of which is known the color and the value
      */
     @Deprecated
-    public Dice getDice(int value, Color color){
+    public Dice getDice(int value, Color color) {
         for (Dice aRestOfStock : restOfStock) {
-            if (aRestOfStock.getColor().equalsColor(color) && aRestOfStock.getValue()==value)
+            if (aRestOfStock.getColor().equalsColor(color) && aRestOfStock.getValue() == value)
                 return aRestOfStock;
-            }
+        }
         return null;
     }
 
     /**
      * removes a chose dice from the round scheme cell
+     *
      * @param dice the chosen dice
      */
     //tested
-    public void removeDice(Dice dice){
+    public void removeDice(Dice dice) {
         restOfStock.remove(dice);
+    }
+
+    /**
+     * method that add an arraylist of dice to a single cell
+     *
+     * @param toAdd the arraylist contains the dices not used during the turn
+     */
+    //DA TESTARE
+    public void setDices(List<Dice> toAdd) {
+
+        if (restOfStock.get(0).getValue() == 0)
+            restOfStock.addAll(0, toAdd);
+        else
+            restOfStock.addAll(toAdd);
     }
 }
 
